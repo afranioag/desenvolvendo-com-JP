dd406ac6f2c4c5b65c25ae79da178b92848b0534
Joao_Paulo <jpbbarboza1987@gmail.com>
2021-04-05 17:52:29 -0300

    modificação do código e viculação do template results.html

--- python/webapp/vsearch4web.py
+++ python/webapp/vsearch4web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from vsearch import search4letters
 
 
@@ -6,12 +6,18 @@ app = Flask(__name__)
 @app.route('/')
 def hello() -> str:
     return 'hello world from flask!'
-@app.route('/search4')
-def do_search() -> str:
-    return ''.join(search4letters('life, the universe, and everything!', 'eiru,!'))
+
+@app.route('/search4', methods=['POST'])
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html', the_phrase = phrase, the_title = title,the_letters= letters, the_results= results,)
+
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to search for letters on the web!')
 
 
-app.run()
+app.run(debug=True)
